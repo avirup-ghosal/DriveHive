96c0e6671a202158ec1727ee1c61be799ae3e726
avirup-ghosal <avirupghosal2023@gmail.com>
2025-05-13 18:46:21 +0530

    Added gitignore file and updated the test file

--- test/CarRental.js
+++ test/CarRental.js
@@ -4,9 +4,17 @@ contract("CarRental",(accounts)=>{
     let carRentalPlatform;
     const owner=accounts[0];
     const renter1=accounts[1];
-})
+
 
 beforeEach(async()=>{
     carRentalPlatform=await CarRental.new();
 });
-
+describe("Add user and car",()=>{
+    it("adds a new user", async()=>{
+        await carRentalPlatform.addUser("Adrian","Walker",{from :renter1});
+        const user=await carRentalPlatform.getUser(renter1);
+        assert.equal(user.name,"Adrian","Incorrect user name");
+        assert.equal(user.lastname,"Walker","Incorrect last name");
+    });
+});
+});
